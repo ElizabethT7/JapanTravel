0440a6c3ba5b0e3ad49979b873dcfaa1a055d891
Liza Timofeeva <tymofieievai@gmail.com>
2022-07-09 00:20:18 +0300

    feat: add footer

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function () {                  //функция вызывает сама себя
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active');
+        }
+    }
+}());
