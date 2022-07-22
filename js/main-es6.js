d1a4f6ae8b8ac414ef20a8065609ece6743b479f
Liza Timofeeva <tymofieievai@gmail.com>
2022-07-22 23:44:49 +0300

    feat: add optimization

--- js/main-es6.js
+++ js/main-es6.js
@@ -13,7 +13,8 @@
 (function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
-    const menuCloseItem = document.querySelector('.header__nav-close')
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link')
 
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav-active');
@@ -21,7 +22,14 @@
 
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header__nav-active');
-    });   
+    });
+    if (window.innerWidth <= 767) {
+        for (i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+            menu.classList.remove('header__nav-active');  
+            });
+        }
+    }
 }());
 
 // Scroll to anchors
