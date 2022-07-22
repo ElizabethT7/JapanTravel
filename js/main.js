3caf8aae9df67018a09d8641e7564c6df9120ea7
Liza Timofeeva <tymofieievai@gmail.com>
2022-07-22 22:24:18 +0300

    feat: add scroll to anhours

--- js/main.js
+++ js/main.js
@@ -21,6 +21,45 @@
 
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header__nav-active');
-    })
+    });   
+}());
+
+// Scroll to anchors
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('.header').clientHeight;  //записываем в переменную высоту header
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;  //удаляем если header не скролится
+        let startPosition = window.pageYOffset;
+        let startTime = null;
     
+        const ease = function(t,b,c,d) {         //отвечает за то, как именно будет анимироваться скрол(ease)
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');  //добавляем данный класс для всех ссылок на которые будем кликать
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
 }());
